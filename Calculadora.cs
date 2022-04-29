56623e4339bfe33b05e62d4949e14037723d66e1
EduardoMen226 <eduardo.mendoza.21isc@tecsanpedro.edu.mx>
2022-04-29 10:28:18 -0500

    Version 1

---
+++ Calculadora.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_U2_Inscripcion
+{
+    internal class Calculadora
+    {
+        //tipoDato identificador (nombreVariable);
+        //string | bool | int | double | float 1.f
+        int sumaNumeroUno = 5;
+        int sumaNumeroDos = 10;
+
+        //Miembro de tipo Propiedad
+        //public tipoDato nombrePropiedad {}
+        public int SumaNumeroUno
+        {
+            get
+            {
+                return sumaNumeroUno;
+            }
+            set
+            {
+                sumaNumeroUno = value;
+            }
+            //get(obtener | lectura)   set(poner | escritura)
+        }
+
+        public int NumeroDosLectura
+        {
+            //leer
+            get { return sumaNumeroDos; }
+        }
+        public int NumeroDosEscritura
+        {
+            //escribir
+            set { sumaNumeroDos = value; }
+        }
+
+
+
+    }
+}
+    
+
---
+++ Folio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_U2_Inscripcion
+{
+    static class Folio
+    {
+
+        public static int numeroFolio = 1;
+
+    }
+}
